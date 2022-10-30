74abd9c59c4f46c32d89d603267cc831b14b5a8e
Ronaldo <ronaldovctor@outlook.com>
2022-10-30 12:37:55 -0300

    api: login controller initial configs

--- api/src/controllers/AuthController.js
+++ api/src/controllers/AuthController.js
@@ -1,6 +1,27 @@
+import { User } from '../../models/User.js'
+import bcrypt from 'bcryptjs'
+
 const controller = {
 	CHECK_TOKEN: async (req, res, next) => {},
-	LOGIN: async (req, res) => {},
+	LOGIN: async (req, res) => {
+		try {
+			const { email, password } = req.body
+			const user = await User.findOne({ email }).lean().exec()
+
+			const checkUser = password == '' || password == null || !user
+			if (!checkUser) {
+				if (bcrypt.compareSync(password, user.password)) {
+					delete user.password
+					console.log(user)
+					return res.status(200).send(user)
+				}
+			} else {
+				return res.status(500).send({ message: 'Wrong e-mail or password' })
+			}
+		} catch (error) {
+			res.status(500).send({ error: error.message, message: 'Server error.' })
+		}
+	},
 }
 
 export default controller
--- api/src/routes.js
+++ api/src/routes.js
@@ -4,8 +4,8 @@ import UserController from './controllers/UserController.js'
 
 const router = Router()
 
-router.post('/user/create', UserController.CREATE_USER)
-router.post('/user/login', AuthController.LOGIN)
+router.post('/create', UserController.CREATE_USER)
+router.post('/login', AuthController.LOGIN)
 
 // router.use(AuthController.CHECK_TOKEN)
 router.patch('/user/edit', UserController.EDIT_USER)
